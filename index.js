a9c428ce0b3de12d79edeef057537cba52f23837
Jaedson Silva <contacanal798@gmail.com>
2022-05-13 19:20:27 -0300

    adding route to make QRcode and returning

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bp = require('body-parser')
+const qr = require('qrcode')
 
 const app = express()
 
@@ -8,6 +9,20 @@ app.use(bp.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.get('/make', (req, res) => {
+    const content = req.query.content
+
+    if(!content || content.length == 0) {
+        console.log('Empty content')
+        res.send('Empty content')
+    }
+
+    qr.toDataURL(content, (err, src) => {
+        if(err) res.send('A error ocurred')
+        res.render('qrcode', {src})
+    })
 })
 
 app.listen(5500, 'localhost', () => {
